098616aee6259229fc363c867adcdcd010f4f881
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-31 23:13:55 +0545

    * implemented the header and screen portions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class App {
-  protected readonly title = signal('habitat-frontend');
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+    this.authService.loggedIn$.subscribe(value=> this.isLoggedIn = value)
+  }
 }
