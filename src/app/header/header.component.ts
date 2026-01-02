d03dc91ee033df316837e705c595b927d4f9f8cc
kushvlshresthv <kushvlshresthv@gmail.com>
2026-01-02 16:14:10 +0545

    * changed the 'create' button to show options on click and styled it as well

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, HostListener, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,4 +9,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showCreateOptions = false;
+  onCreateSvgClick() {
+    this.showCreateOptions = !this.showCreateOptions;
+  }
+
+  constructor(private elementRef: ElementRef) {}
+
+  //remove the options when clicked anywhere in the document
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    //skip if the clicked element is in the header itself
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.showCreateOptions = false;
+    }
+  }
 }
