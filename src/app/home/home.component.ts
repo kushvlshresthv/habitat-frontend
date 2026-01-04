6b3a154751dbb23742490e9ca10d947171f3ccb8
kushvlshresthv <kushvlshresthv@gmail.com>
2026-01-04 19:07:54 +0545

    * improved structure for incomplete-tasks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,21 +8,18 @@ import { IncompleteTasksComponent } from '../incomplete-tasks/incomplete-tasks.c
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor() {
-    console.log("home component has been loaded");
-  }
+  motivationalQuote!: string;
 
   motivationalQuotes: string[] = [
-    "Small steps, done daily, change everything",
+    'Small steps, done daily, change everything',
     "You don't rise to goals, You fall to habits",
-    "Done every day beats perfect someday",
-    "Show up today, Tomorrow will thank you",
-    "Become who you want to be, one habit at a time", 
- ] 
+    'Done every day beats perfect someday',
+    'Show up today, Tomorrow will thank you',
+    'Become who you want to be, one habit at a time',
+  ];
 
-
-  getMotivationalQuote() {
-    return this.motivationalQuotes[Math.floor(Math.random() * this.motivationalQuotes.length)];
+  constructor() {
+    this.motivationalQuote =
+      this.motivationalQuotes[Math.floor(Math.random() * this.motivationalQuotes.length)];
   }
-
 }
